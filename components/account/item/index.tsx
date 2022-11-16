fad84ddb89f7f9d3238915927d591d6869489ca1
유아영 <ahyoung.db@gmail.com>
2022-11-16 22:52:23 +0900

    test: netlify-deploy

---
+++ components/account/item/index.tsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import { AccountItemBox } from './style';
+
+export default function AccountItem() {
+  return <AccountItemBox>아이템</AccountItemBox>;
+}
