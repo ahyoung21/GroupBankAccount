57e3ddcd46adde99f39afcbf9870506621bab796
유아영 <ahyoung.db@gmail.com>
2022-11-28 23:22:25 +0900

    add fontawesome

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { ButtonBox } from './style';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface IButton {
   onClickOpenModal: any;
 }
 
 export default function Button({ onClickOpenModal }: IButton) {
-  return <ButtonBox onClick={onClickOpenModal}>등록</ButtonBox>;
+  return (
+    <ButtonBox onClick={onClickOpenModal}>
+      <FontAwesomeIcon icon={faPen} />
+    </ButtonBox>
+  );
 }
